4c267927eb1444c987c352656614d93902bc1ff9
vivekvks <vshaushi@gmail.com>
2020-08-20 17:21:34 +0530

    Modal - CategoryMaster, ReturnMaster

---
+++ Ecom.API/Models/CategoryReturnMaster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ecom.API.Models
+{
+    public partial class CategoryReturnMaster
+    {
+        public CategoryReturnMaster()
+        {
+            CategoryMaster = new HashSet<CategoryMaster>();
+        }
+
+        public int Id { get; set; }
+        public int NoOfDays { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+
+        public virtual ICollection<CategoryMaster> CategoryMaster { get; set; }
+    }
+}
