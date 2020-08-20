8fc9a1b4400c7f08b46e278cfbcc244467f019ad
vivekvks <vshaushi@gmail.com>
2020-08-20 17:22:50 +0530

    Modal - CategoryMaster, CategoryReturnMaster

--- Ecom.API/Models/CategoryMaster.cs
+++ Ecom.API/Models/CategoryMaster.cs
@@ -5,22 +5,10 @@ namespace Ecom.API.Models
 {
     public partial class CategoryMaster
     {
-        public CategoryMaster()
-        {
-            CategoryAttributeMaster = new HashSet<CategoryAttributeMaster>();
-            CategoryVarianceDetails = new HashSet<CategoryVarianceDetails>();
-            InverseParent = new HashSet<CategoryMaster>();
-        }
 
         public int Id { get; set; }
         public int? ParentId { get; set; }
         public string Name { get; set; }
         public int? ReturnTypeId { get; set; }
-
-        public virtual CategoryMaster Parent { get; set; }
-        public virtual CategoryReturnMaster ReturnType { get; set; }
-        public virtual ICollection<CategoryAttributeMaster> CategoryAttributeMaster { get; set; }
-        public virtual ICollection<CategoryVarianceDetails> CategoryVarianceDetails { get; set; }
-        public virtual ICollection<CategoryMaster> InverseParent { get; set; }
     }
 }
--- Ecom.API/Models/CategoryReturnMaster.cs
+++ Ecom.API/Models/CategoryReturnMaster.cs
@@ -5,16 +5,11 @@ namespace Ecom.API.Models
 {
     public partial class CategoryReturnMaster
     {
-        public CategoryReturnMaster()
-        {
-            CategoryMaster = new HashSet<CategoryMaster>();
-        }
-
+        
         public int Id { get; set; }
         public int NoOfDays { get; set; }
         public string Description { get; set; }
         public bool IsActive { get; set; }
 
-        public virtual ICollection<CategoryMaster> CategoryMaster { get; set; }
     }
 }
