378a415088595ad38e67a4699a5571089c919fb5
Lalji Dhameliya <tthestudy@gmail.com>
2020-08-28 20:13:43 -0700

    add fluent validation on category master and category variance detail

--- Ecom.Models/Request/CategoryMaster/CategoryMasterAddRequest.cs
+++ Ecom.Models/Request/CategoryMaster/CategoryMasterAddRequest.cs
@@ -1,9 +1,20 @@
-﻿namespace Ecom.Models.Request
+﻿using FluentValidation;
+using System.Data;
+
+namespace Ecom.Models.Request
 {
     public class CategoryMasterAddRequest
     {
         public int? ParentId { get; set; }
         public string Name { get; set; }
         public int ReturnTypeId { get; set; }
+    }
+    public class CategoryMasterAddRequestValidator : AbstractValidator<CategoryMasterAddRequest>
+    {
+        public CategoryMasterAddRequestValidator()
+        {
+            RuleFor(categoryMaster => categoryMaster.Name).NotEmpty();
+            RuleFor(categoryMaster => categoryMaster.ReturnTypeId).NotEmpty();
+        }
     }
 }
--- Ecom.Models/Request/CategoryVarianceDetails/CategoryVarianceDetailsAddRequest.cs
+++ Ecom.Models/Request/CategoryVarianceDetails/CategoryVarianceDetailsAddRequest.cs
@@ -1,18 +1,29 @@
 ﻿using FluentValidation;
+using System.Collections.Generic;
 
 namespace Ecom.Models.Request
 {
+    public class CategoryVarianceDetailRequest
+    {
+        public List<CategoryVarianceDetailsAddRequest> CategoryVarianceDetails { get; set; }
+    }
+
     public class CategoryVarianceDetailsAddRequest
     {
         public int CategoryId { get; set; }
         public int? VarianceMasterId { get; set; }
         public string VarianceMasterName { get; set; }
     }
-    public class CategoryVarianceDetailsAddRequestValidator : AbstractValidator<CategoryVarianceDetailsAddRequest>
+    public class CategoryVarianceDetailRequestValidator : AbstractValidator<CategoryVarianceDetailRequest>
     {
-        public CategoryVarianceDetailsAddRequestValidator()
+        public CategoryVarianceDetailRequestValidator()
         {
-
+            //RuleForEach(x => x.Requests).NotNull();
+            RuleForEach(x => x.CategoryVarianceDetails).ChildRules(requests =>
+            {
+                requests.RuleFor(x => x.CategoryId).NotNull();
+                requests.RuleFor(x => x.VarianceMasterId).NotNull().When(x => string.IsNullOrEmpty(x.VarianceMasterName)).WithMessage("VarianceMasterId {CollectionIndex} is required.");
+            });
         }
     }
 }
